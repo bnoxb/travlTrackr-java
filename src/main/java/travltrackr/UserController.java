b695935612c197a5ee1ac80fd4cdd920a72babd2
Robbie <saucy.cooking@gmail.com>
2019-02-25 18:10:07 -0700

    setup lots of stuff and added some routes

---
+++ src/main/java/travltrackr/UserController.java
@@ -0,0 +1,67 @@
+package travltrackr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/api/users")
+    public Iterable<User> index(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/api/users/register")
+    public User createUser(@RequestBody User user){
+        User createdUser = userService.saveUser(user);
+        return createdUser;
+    }
+
+    @PostMapping("/api/users/login")
+    public User checkLogin(@RequestBody User user, HttpSession session)throws Exception{
+
+        bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        User foundUser = userRepository.findByUsername(user.getUsername());
+        System.out.println(foundUser.getPassword() + "and" + user.getPassword());
+        boolean validPass = bCryptPasswordEncoder.matches(user.getPassword(), foundUser.getPassword());
+        if(validPass){
+            session.setAttribute("username", foundUser.getUsername());
+            return foundUser;
+        }else{
+            throw new Exception("incorrect credentials");
+        }
+    }
+
+    @GetMapping("/api/users/{id}")
+    public Optional<User> showUser(@PathVariable Long id){
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser;
+    }
+
+    @PutMapping("/api/users/{id}")
+    public User editUser(@PathVariable Long id, @RequestBody User user){
+        // Dont forget to make a new BCrypt when using bcrypt
+        bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(user.getEmail());
+        Optional<User> userToEdit = userRepository.findById(id);
+        User foundUser = userToEdit.get();
+        foundUser.setUsername(user.getUsername());
+        foundUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        foundUser.setEmail(user.getEmail());
+        System.out.println("foundUser's email: " + foundUser.getEmail());
+        return userRepository.save(foundUser);
+    }
+}
---
+++ src/main/java/travltrackr/WebConfig.java
@@ -0,0 +1,28 @@
+package travltrackr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+}
