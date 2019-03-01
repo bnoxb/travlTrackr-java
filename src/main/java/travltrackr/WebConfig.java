f3768ceeb86d6da9e82cda68c618c5d682a84c4c
Robbie <saucy.cooking@gmail.com>
2019-03-01 12:05:57 -0700

    deploy

--- src/main/java/travltrackr/WebConfig.java
+++ src/main/java/travltrackr/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://safe-brook-56718.herokuapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
