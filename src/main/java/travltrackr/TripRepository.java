d2d8a9821fa91fcfe687b87c161abec675b39f98
Robbie <saucy.cooking@gmail.com>
2019-03-01 11:25:01 -0700

    deploy

--- src/main/java/travltrackr/TripRepository.java
+++ src/main/java/travltrackr/TripRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface TripRepository extends CrudRepository<Trip, Long>{
-    public Iterable<Trip> findAllByUser(Optional<Users> user);
+    public Iterable<Trip> findAllByUser(Optional<Users> users);
 }
